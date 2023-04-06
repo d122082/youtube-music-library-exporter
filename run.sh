957353b64cd130b7598d9719b8ca7f07481b46b7
Hsing-Yu Pan <d122082@users.noreply.github.com>
2023-04-06 10:31:06 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,21 @@
 # /bin/sh
 if [ $1 ]; then
-		html_f=$1
-	else
-		html_f='YouTube Music.html'
-	fi
+                `./metadata-extractor --html-file=$1`
+                `./downloader`
+                 rm -rf downloads/*.opus
+                 for f in ./downloads/*.m4a; do ffmpeg -i "$f" -codec:v copy -codec:a libmp3lame -q:a 2 "${f%.m4a}.mp3"; done
+                 rm -rf ./downloads/*.m4a
+                 rm -rf metadata.json
+                 cat downloader.log
+                 rm -rf downloader.log
+        else
+                `./metadata-extractor --html-file=Youtube\ Music.html`
+                `./downloader`
+                 rm -rf downloads/*.opus
+                 for f in ./downloads/*.m4a; do ffmpeg -i "$f" -codec:v copy -codec:a libmp3lame -q:a 2 "${f%.m4a}.mp3"; done
+                 rm -rf ./downloads/*.m4a
+                 rm -rf metadata.json
+                 cat downloader.log
+                 rm -rf downloader.log
 
-./metadata-extractor  --html-file $html_f
-./downloader
-rm -rf downloader.log
-rm -rf downloads/*.opus
-for f in ./downloads/*.m4a; do ffmpeg -i "$f" -codec:v copy -codec:a libmp3lame -q:a 2 "${f%.m4a}.mp3"; done
-rm -rf ./downloads/*.m4a
-rm -rf metadata.json
+        fi
